21c3a393a4a68b4915d352240790ac50b782d277
Felipe Scherer <felipescherer25@gmail.com>
2020-09-29 20:07:27 -0300

    initial commit

---
+++ CaixaFila.py
@@ -0,0 +1,9 @@
+#Variáveis de caixa guardam quanto tempo o caixa está em uso, 0 está livre
+#Variaveis de cliente guardam quanto tempo ele está esperando na fila
+
+expediente = 8 #Medido em horas
+
+caixa1 = 0
+caixa2 = 0
+caixa3 = 0
+
