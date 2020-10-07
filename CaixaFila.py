9d12b9b25fc54aa18fde72b75a12aa39867bd9ef
Felipe Scherer <felipescherer25@gmail.com>
2020-10-07 00:32:08 -0300

    Adding more simple resolution

--- CaixaFila.py
+++ CaixaFila.py
@@ -1,38 +1,80 @@
 import random #Biblioteca que permite números aleatórios
+from datetime import datetime as dt
+import datetime
 
 def simulador():
-    var_media_cliente = def_retorna_media_cliente_hora() #
-    var_expediente = 8 #Expediente medido em horas
+    var_expediente = 5 #Expediente medido em horas
+    var_media_cliente = def_retorna_media_cliente(var_expediente) #
+    list_caixa_eletronico = ['11:00:00', '11:00:00', '11:00:00'] #def_retorna_quantidade_caixa_eletronico() #Guardaremos os caixas em uma lista que terá os valores 1(caixa sendo usado) ou 0(caixa livre)
     var_numero_cliente = 0 #Guarda o número do cliente atual
     list_clientes = [] #Lista que guarda os clientes com seus horários de chegada
     list_cliente_fila = [] #Fila do banco 
-    var_is_caixa1_free = True #True significa está livre
-    var_is_caixa2_free = True
-    var_is_caixa3_free = True
 
-    for hora in range(var_expediente): #Roda o for na quantidade de horas que o banco fica aberto
-        list_aux = [] 
-        for i in range(var_media_cliente): #Roda o for quantas vezes for a média de clientes por hora    
-            if(random.randint(1,2) == 1): #Verificação para deixar o programa mais aleatório ainda, com margem de erro
-                tempo_chegada = random.randint(1,60)
-                list_aux.append(tempo_chegada)
-            var_numero_cliente += 1
-        list_aux = def_organiza_lista(list_aux)
-        list_aux_2 = []
-        for j in list_aux:
-            list_aux_2.append([hora, j])
 
-        list_clientes.append(list_aux_2)
+    for i in range(var_media_cliente): #Roda o for na quantidade de horas que o banco fica aberto
+        if(random.randint(1,2) == 1):
+            list_clientes.append(def_gera_tempo_chegada())
+            #def_retorna_tempo_operacao_aleatoria()
             
+    list_clientes = sorted(list_clientes) #Organiza a lista de tempos de chegada dos clientes
+    print("-"*80)
+    print(f"{len(list_clientes)} clientes foram ao banco nessa simulação com {len(list_caixa_eletronico)} caixas eletrônicos")
+    
+
 
     print(list_clientes)
+    formato_tempo = '%H:%M:%S'
+    time_zero = dt.strptime('00:00:00', formato_tempo)
+    for i in range(len(list_clientes)): #For da verdade
+        tem_caixa_livre = False
+        indice_caixa = 0
+        for j in range(len(list_caixa_eletronico)):
+            t1 = dt.strptime(list_caixa_eletronico[j], formato_tempo)
+            t2 = dt.strptime(list_clientes[i], formato_tempo)
+            print(t2-t1)
+            if ((t2 - t1).time() >= time_zero.time()) : #Achou caixa livre
+                tem_caixa_livre = True
+                indice_caixa = j
 
-def def_retorna_media_cliente_hora(): #Retorna a média de clientes que entram no banco a cada hora
+        if(tem_caixa_livre): #Achou caixa livre, aqui iremos fazer a operação
+            print(f'O caixa {indice_caixa} está vazio e será usado pelo cliente {i + 1} que chegou {list_clientes[i]}')
+            list_caixa_eletronico[indice_caixa] = 1
+        else:
+            print(f'Todos os caixas estão ocupados, o cliente {i + 1} está esperando na fila')
+
+        
+   
+    tempo_inicial = '11:00:00'
+    tempo_final = '16:00:00'
+
+    
+    t1 =  dt.strptime(list_clientes[1], FMT)
+    t2 = dt.strptime('00:00:40', FMT)
+    
+    tdelta = (t1- time_zero + t2).time()
+    #tdelta = datetime.timedelta(hours=11, minutes=0, seconds=0) + datetime.timedelta(seconds=40)
+
+    print(tdelta)
+
+def def_gera_tempo_chegada():
+    # generate random number scaled to number of seconds in a day
+    # (6*60*60) = 18000
+    
+    tempo_random = int(random.random()*18000) #random.random() gera um float 0.alguma coisa
+    
+    horas   = int(tempo_random/3600)
+    minutos = int((tempo_random - horas*3600)/60)
+    segundos = tempo_random - horas*3600 - minutos*60
+    
+    tempo_final = '%02d:%02d:%02d' % (horas + 11, minutos, segundos) #Somamos 11 as horas, porque o banco abre as 11
+    return tempo_final
+
+def def_retorna_media_cliente(var_expediente): #Retorna a média de clientes que entram no banco a cada minuto
     x = ""
     while(True):
         try: 
-            x = int(input("\033[33mQual a média de clientes por hora no banco? \033[m"))
-            if x <= 0 or x > 50:
+            x = int(input(f"\033[33mQual a média de clientes por dia ({var_expediente} horas) no banco? \033[m"))
+            if x <= 0: #or x > 100:
                 raise Exception()
             break
         except:
@@ -40,22 +82,91 @@ def def_retorna_media_cliente_hora(): #Retorna a média de clientes que entram n
             print("Escreva um número inteiro de \033[1;31m0 a 50\033[m")
     return x
 
-def def_organiza_lista(lista): #Organiza a lista de números passada
-    def buscaMenor(array):
-        menor_valor = array[0]
-        menor_indice = 0
+def def_retorna_quantidade_caixa_eletronico(): #Pede a quantidade de caixas eletronicos que existe nessa simulação
+    x = ""
+    while(True):
+        try: 
+            x = int(input(f"\033[33mQual a quantidade de caixas eletrônicos no banco? \033[m"))
+            if x <= 0 or x > 10:
+                raise Exception()
+            break
+        except:
+            print("\033[31mIsso não é um número válido")
+            print("Escreva um número inteiro de \033[1;31m0 a 10\033[m")
+    lista = []
+    for i in range(x):
+        lista.append(0)
+    return lista
 
-        for i in range(1, len(array)):
-            if(array[i] < menor_valor):
-                menor_valor = array[i]
-                menor_indice = i
-        return menor_indice
-
-    lista_ordenado = []
-    for i in range(len(lista)):
-        menor = buscaMenor(lista)
-        lista_ordenado.append(lista.pop(menor))
-    return lista_ordenado
-
+def def_retorna_tempo_operacao_aleatoria(): #Retorna o tempo gasto de uma operação aletória do caixa eletrônico
+    var_operacao = random.randint(0,4)
+    if(var_operacao == 0):
+        #Operação escolhida igual a Saldo, com o tempo de 10 segundos
+        print("Operação Saldo, com o tempo de 10 segundos")
+        return 10
+    elif(var_operacao == 1):
+        #Operação escolhida igual a Saque, com o tempo de 20 segundos
+        print("Operação Saque, com o tempo de 20 segundos")
+        return 20
+    elif(var_operacao == 2):
+        #Operação escolhida igual a Aplicação, com o tempo de 30 segundos
+        print("Operação Aplicação, com o tempo de 30 segundos")
+        return 30
+    elif(var_operacao == 3):
+        #Operação escolhida igual a Extrato Semanal, com o tempo de 40 segundos
+        print("Operação Extrato Semanal, com o tempo de 40 segundos")
+        return 40
+    elif(var_operacao == 4):
+        #Operação escolhida igual a Extrato Mensal, com o tempo de 50 segundos
+        print("Operação Extrato Mensal, com o tempo de 50 segundos")
+        return 50
 
 simulador()
+
+
+'''
+def def_organiza_lista(lista): #Organiza a lista de números passada
+    def busca_menor(lista,indice): #Indice valor que deverá ser contado para ser ordenado
+        menor_valor = int(lista[0][indice-1:indice+1])
+        menor_indice = 0
+        for i in range(1,len(lista)):
+            if int(lista[i][indice-1:indice+1]) < menor_valor: #segundo número das horas
+                menor_valor = int(lista[i][indice-1:indice+1])
+                menor_indice = i
+        return menor_indice
+            
+    lista_ordenada = []
+    
+    for i in range(len(lista)):
+        menor = busca_menor(lista, 1)
+        lista_ordenada.append(lista.pop(menor))
+        
+
+    for i in range(len(lista)):
+        if lista[i-1][0:2] == lista[i][0:2]:
+            print(lista[i-1][0:2])
+            print(lista[i][0:2])
+            menor = busca_menor(lista, 4)
+            lista_ordenada.append(lista.pop(menor))
+
+    for i in range(len(lista)):
+        print("oi")
+        menor = busca_menor(lista, 7)
+        lista_ordenada.append(lista.pop(menor))       
+        
+    return lista_ordenada'''
+
+'''
+list_aux_minutos = [] 
+        for i in range(var_media_cliente): #Roda o for quantas vezes for a média de clientes por hora    
+            if(random.randint(1,2) == 1): #Verificação para deixar o programa mais aleatório ainda, com margem de erro
+                tempo_chegada_minutos = random.randint(1,60)
+                list_aux_minutos.append(tempo_chegada_minutos)
+            var_numero_cliente += 1             
+        list_aux_minutos = def_organiza_lista(list_aux_minutos)
+        list_aux_2 = []
+        for j in list_aux_minutos:
+            var_segundo_random = random.randint(1,60)
+            list_aux_2.append([hora, j])
+
+        list_clientes.append(list_aux_2)'''
---
+++ CaixaFilaCronometro.py
@@ -0,0 +1,126 @@
+#Simulador que retorna o tempo médio que clientes esperam na fila para usar o caixa eletrônico
+
+from random import randint
+
+def simula():
+    expedienteDoBanco = calculaExpediente() * 60 *60 #Medido em segundos
+    #mediaClientes = calculaMediaClientes()
+    caixasEletronicos = calculaCaixasEletronicos()
+    tempoDecorrido = 0
+    totalClientes = 0
+    totalTempoNaFila = 0
+    filaClientes = []
+    f = open("relatorio.txt", "w")
+    while(True):
+        #Para diminuir a quantidade de vezes que o programa roda, foi decidido que o programa irá rodar de 10 em 10 segundos
+
+        print('-'*60)
+        print(f'\033[35mMomento {tempoDecorrido}s\033[m')
+        f.write("\n")
+        f.write(f'Momento {tempoDecorrido}s\n') #Escreve no relatorio.txt 
+        if(tempoDecorrido <= expedienteDoBanco):
+            if(randint(1,2) == 1): #Aleatoriza a quantidade de cliente
+                print(f"Cliente entrou no banco no tempo {tempoDecorrido}")
+                f.write(f"Cliente entrou no banco no tempo {tempoDecorrido}\n")
+                filaClientes.append(tempoDecorrido)
+                totalClientes += 1
+
+        for i in range(len(caixasEletronicos)):
+            if(caixasEletronicos[i] == 0):
+                print(f"Caixa {i} está livre")
+                f.write(f"Caixa {i} esta livre\n")
+                if (len(filaClientes) != 0): #Fila não está vazia
+                    print(f"Cliente que chegou no horario {filaClientes[0]} está indo ao caixa {i}")
+                    f.write(f"Cliente que chegou no horario {filaClientes[0]} esta indo ao caixa {i}")
+                    totalTempoNaFila += tempoDecorrido - filaClientes[0]
+                    filaClientes.pop(0)
+                    caixasEletronicos[i] = escolheOperacao()
+            else:
+                caixasEletronicos[i] -= 10
+
+        tempoDecorrido += 10
+        if((tempoDecorrido >= expedienteDoBanco) and (len(filaClientes) == 0)):
+            break
+        
+    f.close
+
+    print(f"A quantidade de clientes que foram ao banco nessa simulação foi {totalClientes}")
+    print(f"A média de espera na fila foi de {totalTempoNaFila/totalClientes}")
+
+def escolheOperacao():
+    operacao = randint(0,4)
+    if(operacao == 0):
+        #Saldo +10 s
+        print(f'Cliente escolheu ver seu saldo que levou 10 segundos')
+        return 10
+    elif(operacao == 1):
+        #Saque +20 s
+        print(f'Cliente escolheu fazer um saque que levou 20 segundos')
+        return 20
+    elif(operacao == 2):
+        #Aplicação + 30s
+        print(f'Cliente escolheu fazer uma aplicação que levou 30 segundos')
+        return 30
+    elif(operacao == 3):
+        #Extrato Semanal +40 s
+        print(f'Cliente escolheu fazer um extrato semanal que levou 40 segundos')
+        return 40
+    elif(operacao == 4):
+        #Extrato Mensal + 50s
+        print(f'Cliente escolheu fazer um extrato mensal que levou 50 segundos')
+        return 50
+
+def calculaCaixasEletronicos():
+    x = ""
+    while(True):
+        try: 
+            x = int(input(f"\033[33mQual a quantidade de caixas eletrônicos no banco? \033[m"))
+            if x <= 0 or x >= 10:
+                raise Exception()
+            break
+        except:
+            print("\033[31mIsso não é um número válido")
+            print("Escreva um número inteiro de \033[1;31m0 a 10\033[m")
+    lista = []
+    for i in range(x):
+        lista.append(0)
+    print(lista)
+    return lista    
+
+#def tentaCriarCliente(mediaClientes): #Aleatoriza a media de clientes, ou seja, a cada 10s nem sempre vai vir a media certinha
+    qntClientes = 0
+    for i in range(mediaClientes):
+        if(randint(1,2) == 1):
+            qntClientes += 1
+    return qntClientes
+
+def calculaMediaClientes():
+    x = ""
+    while(True):
+        try: 
+            x = int(input(f"\033[33mQual a média de clientes que vão ao banco a cada 10 segundos? \033[m"))
+            if x <= 0 or x > 10:
+                raise Exception()
+            break
+        except:
+            print("\033[31mIsso não é um número válido")
+            print("Escreva um número inteiro de \033[1;31m1 a 10\033[m")
+    return x
+
+def calculaExpediente(): 
+    x = ""
+    while(True):
+        try: 
+            x = int(input(f"\033[33mPor quantas horas o banco fica aberto? \033[m"))
+            if x <= 0 or x > 6:
+                raise Exception()
+            break
+        except:
+            print("\033[31mIsso não é um número válido")
+            print("Escreva um número inteiro de \033[1;31m1 a 6\033[m")
+            print("[31mMais que 6 horas pode travar sua IDE\033[m")
+    return x
+
+
+simula()
+
